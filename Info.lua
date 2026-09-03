4d877a4eca5ec967f41476ac040f43fe9f0788bf
Liodden73 <ole@liodden.com>
2026-09-03 08:25:22 +0200

    v0.9.160: sort countries alphabetically; scrolled_view for continents > 4 countries

--- Info.lua
+++ Info.lua
@@ -21,5 +21,5 @@ return {
                 },
         },
 
-        VERSION = { major = 0, minor = 9, revision = 159, build = 0 },
+        VERSION = { major = 0, minor = 9, revision = 160, build = 0 },
 }
