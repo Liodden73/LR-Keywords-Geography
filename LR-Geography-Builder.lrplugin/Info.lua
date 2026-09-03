6e6ef88e2292ffca1ba8ed7650e005b6cb4b7905
Liodden73 <ole@liodden.com>
2026-09-03 14:51:15 +0200

    v0.9.173: add 4 sub-antarctic islands to France remoteIslandNames

--- LR-Geography-Builder.lrplugin/Info.lua
+++ LR-Geography-Builder.lrplugin/Info.lua
@@ -21,5 +21,5 @@ return {
                 },
         },
 
-        VERSION = { major = 0, minor = 9, revision = 172, build = 0 },
+        VERSION = { major = 0, minor = 9, revision = 173, build = 0 },
 }
