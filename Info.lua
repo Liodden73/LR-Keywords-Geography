c7ca764abb419c24250eb3b28689b5ce6ad02c50
Liodden73 <ole@liodden.com>
2026-09-03 08:29:27 +0200

    v0.9.161: South Africa remote islands — replace archipelago with individual islands + add coastal islands

--- Info.lua
+++ Info.lua
@@ -21,5 +21,5 @@ return {
                 },
         },
 
-        VERSION = { major = 0, minor = 9, revision = 160, build = 0 },
+        VERSION = { major = 0, minor = 9, revision = 161, build = 0 },
 }
