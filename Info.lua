009f97a53d5bec9052bf7fa6132afb9314afab32
Liodden73 <liodden73@users.noreply.github.com>
2026-09-06 09:08:25 +0000

    v0.9.215 – 14 nye Svalbard-polygoner (Isfjorden-system + Van Mijenfjorden)

--- Info.lua
+++ Info.lua
@@ -21,5 +21,5 @@ return {
                 },
         },
 
-        VERSION = { major = 0, minor = 9, revision = 214, build = 0 },
+        VERSION = { major = 0, minor = 9, revision = 215, build = 0 },
 }
